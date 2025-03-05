3ed19a57fc1f38e1b5ae60bf54e1b883029f07e4
Olga Bernal <obersan6@gmail.com>
2025-03-04 20:06:32 -0500

    Updated database connection to use Supabase

--- src/application/secret_keys.py
+++ src/application/secret_keys.py
@@ -4,5 +4,8 @@ import secrets
 # Generate a 32-byte SECRET_KEY
 SECRET_KEY = secrets.token_hex(32)
 
-# Define my SQLALCHEMY_DATABASE_URI
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Caccolino5@localhost/diagnosis_db'
+# Define my SQLALCHEMY_DATABASE_URI for development
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Caccolino5@localhost/diagnosis_db'
+
+# Define my SQLALCHEMY_DATABASE_URI for deployment
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres.rdsnxgihfwemtrdynqol@aws-0-eu-west-2.pooler.supabase.com:6543/postgres"
